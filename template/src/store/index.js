5003be6a8d8fd77a777733bab823744a6f9fb1cc
zhangsuzhen <zhangsuzhen@innotechx.com>
2018-04-11 16:37:01 +0800

    store修改

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -3,7 +3,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
-const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
     state: {},
     getters: {},
