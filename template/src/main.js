275a53ff3a19e7813f82a807664c574ced60e017
zhangsuzhen <zhangsuzhen@innotechx.com>
2018-04-11 16:29:11 +0800

    main.js文件修改

--- template/src/main.js
+++ template/src/main.js
@@ -13,7 +13,6 @@ import Store from './store'
 {{#elementUI}}
 import ElmentUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import '@/assets/css/base.scss'
 Vue.use(ElmentUI)
 {{/elementUI}}
 Vue.config.productionTip = false
